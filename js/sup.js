761f58441df8f8219d50ccca60806dd500043bce
Distortion <sebastian99812@outlook.com>
2026-01-05 21:39:20 -0500

    Update sup.js

--- js/sup.js
+++ js/sup.js
@@ -1,3 +1,10 @@
-fetch("https://purple-rain-c7cb.sebastian19852006.workers.dev/", {
-  method: "POST"
-});
+const VISIT_KEY = "visit_sent";
+
+if (!localStorage.getItem(VISIT_KEY)) {
+  localStorage.setItem(VISIT_KEY, "1");
+
+  fetch("https://purple-rain-c7cb.sebastian19852006.workers.dev/", {
+    method: "POST",
+    keepalive: true
+  }).catch(() => {});
+}
